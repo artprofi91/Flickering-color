ccdad9125009351ddf2b1e5b559b76f0d5f111e9
Артем Трегубчак <Artprofi91@MacBook-Pro-Artem.local>
2017-08-14 00:03:21 -0500

    flickering text effect

---
+++ code.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function() {
+    var nodes = document.querySelectorAll(".words");
+    for (var i = 0; i < nodes.length; i++) {
+        var words = nodes[i].innerText.split("|");
+        var html = "";
+        for (var i2 = 0; i2 < words.length; i2++) {
+            html += "<span>" + words[i2] + "</span>"
+        }
+        nodes[i].innerHTML = html;
+    }
+});
